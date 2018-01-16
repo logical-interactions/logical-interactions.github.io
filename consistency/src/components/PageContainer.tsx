0c5ca98d5675817bbe9f56b3f0b9f4a7d37b4768
Yifan Wu <yifan1030@gmail.com>
2018-01-16 12:26:22 -0800

    setting up the consistency post

---
+++ consistency/src/components/PageContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import MapZoom from "./MapZoom";
+
+export default class PageContainer extends React.Component<undefined, undefined> {
+  render() {
+    return (<div>
+      <MapZoom/>
+    </div>);
+  }
+}
